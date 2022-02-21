db40628d47a815e5d7ed122d8a125f4e6d91d93d
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2022-02-20 20:20:31 -0500

    Rebase Hours

--- src/main/java/edu/wpi/GoldenGandaberundas/controllers/foodDeliveryController.java
+++ src/main/java/edu/wpi/GoldenGandaberundas/controllers/foodDeliveryController.java
@@ -1,38 +1,55 @@
 package edu.wpi.GoldenGandaberundas.controllers;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDrawer;
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.Food;
+import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodRequest;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.util.Callback;
-import org.controlsfx.control.GridCell;
-import org.controlsfx.control.GridView;
-import org.controlsfx.control.ListSelectionView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class foodDeliveryController {
 
-  @FXML ListSelectionView<Food> bruh;
-  @FXML JFXButton homeBtn;
-  @FXML ChoiceBox restaurantCB;
-  @FXML Label menuLabel;
+  @FXML TableView foodMenuTable;
+  @FXML TableColumn<Food, Integer> foodID;
+  @FXML TableColumn<Food, String> foodType;
+  @FXML TableColumn<Food, Double> price;
+  @FXML TableColumn<Food, Boolean> inStock;
 
-
-
-  @FXML JFXButton currentOrdersBtn; // Cool Sliding Button
-  @FXML JFXDrawer drawer; // sliding side menu
-  @FXML VBox drawerBox; // Cool Sliding Table
-  @FXML TableView ordersTbl; // Service Table
-
+  @FXML TableView foodRequestsTable;
+  @FXML TableColumn<FoodRequest, Integer> requestID;
+  @FXML TableColumn<FoodRequest, String> locationID;
+  @FXML TableColumn<FoodRequest, Integer> timeStart;
+  @FXML TableColumn<FoodRequest, Integer> timeEnd;
+  @FXML TableColumn<FoodRequest, Integer> patientID;
+  @FXML TableColumn<FoodRequest, Integer> empInitiated;
+  @FXML TableColumn<FoodRequest, Integer> empCompleter;
+  @FXML TableColumn<FoodRequest, String> requestStatus;
+  @FXML TableColumn<FoodRequest, String> notes;
+  @FXML TableColumn<FoodRequest, Integer> foodTblID;
+  @FXML TableColumn<FoodRequest, Integer> quantity;
 
   @FXML
   public void initialize() {
 
+    foodID.setCellValueFactory(new PropertyValueFactory<Food,Integer>("foodID"));
+
+    /**
+     * Sets the cell value factories for the requests table each of the cell value factories make a
+     * new property value factory which takes in the exact name of the value in the class of
+     * foodrequest
+     */
+    requestID.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("requestID"));
+    locationID.setCellValueFactory(new PropertyValueFactory<FoodRequest, String>("locationID"));
+    timeStart.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("timeStart"));
+    timeEnd.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("timeEnd"));
+    patientID.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("patientID"));
+    empInitiated.setCellValueFactory(
+        new PropertyValueFactory<FoodRequest, Integer>("empInitiated"));
+    empCompleter.setCellValueFactory(
+        new PropertyValueFactory<FoodRequest, Integer>("empCompleter"));
+    requestStatus.setCellValueFactory(
+        new PropertyValueFactory<FoodRequest, String>("requestStatus"));
+    notes.setCellValueFactory(new PropertyValueFactory<FoodRequest, String>("notes"));
+    foodTblID.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("foodID"));
+    quantity.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("quantity"));
   }
-
-
 }
