56f41b54a9c8f827915f9bc0a71a608174b16d5d
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2022-02-20 22:20:34 -0500

    Food Delivery Being Created

--- src/main/java/edu/wpi/GoldenGandaberundas/controllers/FoodControllers/foodDeliveryController.java
+++ src/main/java/edu/wpi/GoldenGandaberundas/controllers/FoodControllers/foodDeliveryController.java
@@ -1,12 +1,24 @@
-package edu.wpi.GoldenGandaberundas.controllers;
+package edu.wpi.GoldenGandaberundas.controllers.FoodControllers;
 
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.Food;
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodRequest;
+import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodRequestTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.Locations.Location;
+import edu.wpi.GoldenGandaberundas.tableControllers.Locations.LocationTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.Patients.Patient;
+import edu.wpi.GoldenGandaberundas.tableControllers.Patients.PatientTbl;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.controlsfx.control.SearchableComboBox;
 
 public class foodDeliveryController {
+
+  @FXML TableView menuTable;
+  @FXML TableColumn<Food, String> foodName;
 
   @FXML TableView foodMenuTable;
   @FXML TableColumn<Food, Integer> foodID;
@@ -27,10 +39,17 @@ public class foodDeliveryController {
   @FXML TableColumn<FoodRequest, Integer> foodTblID;
   @FXML TableColumn<FoodRequest, Integer> quantity;
 
+  @FXML SearchableComboBox<Integer> patientComboBox;
+  @FXML SearchableComboBox<String> locationComboBox;
+  @FXML TextArea noteField;
+
   @FXML
   public void initialize() {
 
-    foodID.setCellValueFactory(new PropertyValueFactory<Food,Integer>("foodID"));
+    foodID.setCellValueFactory(new PropertyValueFactory<Food, Integer>("foodID"));
+    foodType.setCellValueFactory(new PropertyValueFactory<Food, String>("foodType"));
+    //price.setCellValueFactory(new PropertyValueFactory<Food, Double>("price"));
+    inStock.setCellValueFactory(new PropertyValueFactory<Food, Boolean>("inStock"));
 
     /**
      * Sets the cell value factories for the requests table each of the cell value factories make a
@@ -51,5 +70,30 @@ public class foodDeliveryController {
     notes.setCellValueFactory(new PropertyValueFactory<FoodRequest, String>("notes"));
     foodTblID.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("foodID"));
     quantity.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("quantity"));
+
+    ArrayList<Integer> patientIDs = new ArrayList<Integer>();
+    for (Patient p : PatientTbl.getInstance().readTable()) {
+      patientIDs.add(p.getPatientID());
+    }
+    patientComboBox.setItems(FXCollections.observableArrayList(patientIDs));
+
+    ArrayList<String> locations = new ArrayList<String>();
+    for (Location l : (ArrayList<Location>) LocationTbl.getInstance().readTable()) {
+      locations.add(l.getNodeID());
+    }
+    locationComboBox.setItems(FXCollections.observableArrayList(locations));
+
+    refresh();
+  }
+
+  public void refresh() {
+    foodMenuTable.getItems().setAll(FoodTbl.getInstance().readTable());
+    foodRequestsTable.getItems().setAll(FoodRequestTbl.getInstance().readTable());
+  }
+
+  public void addToCart() {
+    if (foodMenuTable.getSelectionModel().getSelectedItem() != null) {
+      Food addFood = (Food) foodMenuTable.getSelectionModel().getSelectedItem();
+    }
   }
 }
