9cb09e7e5acea992639f17f0cad1b369a34bd745
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2022-02-21 02:09:09 -0500

    FOOD IS DONE AND DOCED

--- src/main/java/edu/wpi/GoldenGandaberundas/controllers/FoodControllers/foodDeliveryController.java
+++ src/main/java/edu/wpi/GoldenGandaberundas/controllers/FoodControllers/foodDeliveryController.java
@@ -1,5 +1,6 @@
 package edu.wpi.GoldenGandaberundas.controllers.FoodControllers;
 
+import edu.wpi.GoldenGandaberundas.App;
 import edu.wpi.GoldenGandaberundas.CurrentUser;
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.Food;
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodRequest;
@@ -10,11 +11,19 @@ import edu.wpi.GoldenGandaberundas.tableControllers.Locations.LocationTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.Patients.Patient;
 import edu.wpi.GoldenGandaberundas.tableControllers.Patients.PatientTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.Requests.RequestTable;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.controlsfx.control.SearchableComboBox;
 
 public class foodDeliveryController {
@@ -56,6 +65,10 @@ public class foodDeliveryController {
   @FXML
   public void initialize() {
 
+    /**
+     * Sets the cell value factories for the food menu Each of them uses the values from the Food
+     * Object
+     */
     foodID.setCellValueFactory(new PropertyValueFactory<Food, Integer>("foodID"));
     foodType.setCellValueFactory(new PropertyValueFactory<Food, String>("foodType"));
     // price.setCellValueFactory(new PropertyValueFactory<Food, Double>("price"));
@@ -85,21 +98,31 @@ public class foodDeliveryController {
     foodTblID.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("foodID"));
     quantity.setCellValueFactory(new PropertyValueFactory<FoodRequest, Integer>("quantity"));
 
+    /**
+     * Sets the cell value factories for the "cart" table Each makes one out of the FoodMenuItem
+     * included in the FoodController package
+     */
     foodNameCart.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, String>("foodName"));
     quantityCart.setCellValueFactory(new PropertyValueFactory<FoodMenuItem, Integer>("quantity"));
 
+    /** Gets the patients from the patient table and sets them to the patient combobox */
     ArrayList<Integer> patientIDs = new ArrayList<Integer>();
     for (Patient p : PatientTbl.getInstance().readTable()) {
       patientIDs.add(p.getPatientID());
     }
     patientComboBox.setItems(FXCollections.observableArrayList(patientIDs));
 
+    /** Gets the locations from the locations table and sets them to the location combobox */
     ArrayList<String> locations = new ArrayList<String>();
     for (Location l : (ArrayList<Location>) LocationTbl.getInstance().readTable()) {
       locations.add(l.getNodeID());
     }
     locationComboBox.setItems(FXCollections.observableArrayList(locations));
 
+    /**
+     * This function is what allows the current "cart" menu to be double clicked and remove one
+     * quantity of an item from a cart.
+     */
     menuTable.setOnMouseClicked(
         e -> {
           if (e.getClickCount() > 1) {
@@ -107,9 +130,44 @@ public class foodDeliveryController {
           }
         });
 
+    /**
+     * This function is what allows the foodRequeststable to be double clicked and open the edit
+     * form
+     */
+    foodRequestsTable.setOnMouseClicked(
+        e -> {
+          if (e.getClickCount() > 1) {
+            onEdit();
+          }
+        });
+
     refresh();
   }
 
+  /**
+   * First, gets the selected item from the foodrequests table, then loads the edit form for food
+   * and uses its editForm "constructor" to pass in the selectd item. Then opens
+   */
+  public void onEdit() {
+    if (foodRequestsTable.getSelectionModel().getSelectedItem() != null) {
+      FoodRequest selectedItem =
+          (FoodRequest) foodRequestsTable.getSelectionModel().getSelectedItem();
+      try {
+        FXMLLoader load = new FXMLLoader(App.class.getResource("views/editFoodReqForm.fxml"));
+        AnchorPane editForm = load.load();
+        editFoodReqFormController edit = load.getController();
+        edit.editForm(RequestTable.getInstance().getEntry(selectedItem.getPK().get(0)));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(editForm));
+        stage.show();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  /** Refreshes the tables */
   public void refresh() {
     ArrayList<Food> filteredFoods = FoodTbl.getInstance().readTable();
 
@@ -118,6 +176,11 @@ public class foodDeliveryController {
     menuTable.getItems().setAll(currentMenu);
   }
 
+  /**
+   * When button is pressed, checks to see if there is a selected item on the foodMenuTable, if
+   * there is it will add an instance of it to the "cart" if it doesnt exist. If it does exist it
+   * will increase its quantity and leave the function
+   */
   public void addToCart() {
     if (foodMenuTable.getSelectionModel().getSelectedItem() != null) {
       Food addFood = (Food) foodMenuTable.getSelectionModel().getSelectedItem();
@@ -134,6 +197,10 @@ public class foodDeliveryController {
     }
   }
 
+  /**
+   * When remove from cart button is pressed, checks to see if there is a selected item on the
+   * "menu", no matter the quantity it will remove if if it exists
+   */
   public void removeFromCart() {
     if (menuTable.getSelectionModel().getSelectedItem() != null) {
       currentMenu.remove(menuTable.getSelectionModel().getSelectedItem());
@@ -141,13 +208,25 @@ public class foodDeliveryController {
     refresh();
   }
 
+  /**
+   * Is the method that is used when the "cart" table is double clicked Decreases the quantity of
+   * the menu item by one unless it decreases it to 0 where it will delete it instead.
+   */
   public void removeOneFromCart() {
     if (menuTable.getSelectionModel().getSelectedItem() != null) {
       ((FoodMenuItem) menuTable.getSelectionModel().getSelectedItem()).decreaseQuantity();
+      if (((FoodMenuItem) menuTable.getSelectionModel().getSelectedItem()).getQuantity() == 0) {
+        removeFromCart();
+      }
     }
+
     refresh();
   }
 
+  /**
+   * Submits a request in the requests table when there is stuff in the "cart"
+   * Only things not auto-generated are the patientID, the locationID and the notes
+   */
   public void submit() {
     int requestNum =
         RequestTable.getInstance().readTable().size() - 1 < 0
@@ -178,6 +257,95 @@ public class foodDeliveryController {
       RequestTable.getInstance().addEntry(foodRequest);
     }
     currentMenu.clear();
+    refresh();
+  }
+
+  /**
+   * Opens a file directory and prompts the user to
+   * create a backup for the food menu database
+   */
+  @FXML
+  public void backupFood() {
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    directoryChooser.setTitle("Select Back Up Food File");
+    Stage popUpDialog = new Stage();
+    File selectedFile = directoryChooser.showDialog(popUpDialog);
+    popUpDialog.show();
+
+    if (selectedFile != null) {
+      FoodTbl.getInstance().createBackup(new File(selectedFile.toString() + "\\foodTblBackUp.csv"));
+    } else {
+      System.err.println("BACK UP FILE SELECTED DOES NOT EXIST");
+    }
+    popUpDialog.close();
+  }
+
+  /**
+   * Opens a file directory and prompts the user to
+   * create a backup for the food requests database
+   */
+  @FXML
+  public void backupRequests() {
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    directoryChooser.setTitle("Select Back Up Requests File");
+    Stage popUpDialog = new Stage();
+    File selectedFile = directoryChooser.showDialog(popUpDialog);
+    popUpDialog.show();
+
+    if (selectedFile != null) {
+      FoodRequestTbl.getInstance()
+          .createBackup(new File(selectedFile.toString() + "\\medLaundryRequestsBackUp.csv"));
+    } else {
+      System.err.println("BACK UP FILE SELECTED DOES NOT EXIST");
+    }
+    popUpDialog.close();
+  }
+
+  /**
+   * Opens a file directory and prompts the user to
+   * load a DB File Backup for the food menu
+   */
+  @FXML
+  public void loadDBFood() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Select Back Up Food File To Load");
+    fileChooser
+        .getExtensionFilters()
+        .add(new FileChooser.ExtensionFilter("Comma Seperated Values", "*.csv", "*.CSV"));
+    Stage popUpDialog = new Stage();
+    File selectedFile = fileChooser.showOpenDialog(popUpDialog);
+    popUpDialog.show();
+    if (selectedFile != null) {
+      System.out.println(selectedFile.toString());
+      FoodTbl.getInstance().loadBackup(selectedFile.toString());
+    } else {
+      System.err.println("BACK UP FILE SELECTED DOES NOT EXIST");
+    }
+    popUpDialog.close();
+    refresh();
+  }
+
+  /**
+   * Opens a file directory and prompts the user to
+   * load a DB File Backup for the food requests table
+   */
+  @FXML
+  public void loadDBRequests() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Select Back Up Food Requests File To Load");
+    fileChooser
+        .getExtensionFilters()
+        .add(new FileChooser.ExtensionFilter("Comma Seperated Values", "*.csv", "*.CSV"));
+    Stage popUpDialog = new Stage();
+    File selectedFile = fileChooser.showOpenDialog(popUpDialog);
+    popUpDialog.show();
+    if (selectedFile != null) {
+      System.out.println(selectedFile.toString());
+      FoodRequestTbl.getInstance().loadBackup(selectedFile.toString());
+    } else {
+      System.err.println("BACK UP FILE SELECTED DOES NOT EXIST");
+    }
+    popUpDialog.close();
     refresh();
   }
 }
