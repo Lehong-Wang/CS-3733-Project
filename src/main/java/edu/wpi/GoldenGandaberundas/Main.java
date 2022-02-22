d92a9339fd6744a4ea20117fa432cf2cffb1cf8b
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2022-02-22 01:56:18 -0500

    Some Minor Fices

--- src/main/java/edu/wpi/GoldenGandaberundas/Main.java
+++ src/main/java/edu/wpi/GoldenGandaberundas/Main.java
@@ -7,6 +7,7 @@ import edu.wpi.GoldenGandaberundas.tableControllers.AudioVisualService.AudioVisu
 import edu.wpi.GoldenGandaberundas.tableControllers.AudioVisualService.AudioVisualTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.ComputerService.ComputerRequestTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.ComputerService.ComputerTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.EmployeeObjects.Credential;
 import edu.wpi.GoldenGandaberundas.tableControllers.EmployeeObjects.CredentialsTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.EmployeeObjects.EmployeeTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.EmployeePermissionTbl;
@@ -32,14 +33,14 @@ public class Main {
     System.out.println(PathTbl.getInstance().objList);
     // Initializes the database tables in memory
 
-    EmployeeTbl.getInstance(); // .loadBackup("BackupsCSVs/employeeTbl.csv");
+    EmployeeTbl.getInstance().loadBackup("BackupsCSVs/employeeTbl.csv");
     LocationTbl.getInstance();
     PatientTbl.getInstance();
     LaundryTbl.getInstance();
     MedEquipmentTbl.getInstance();
     MedicineTbl.getInstance();
     GiftTbl.getInstance();
-    PermissionTbl.getInstance(); // .loadBackup("BackupsCSVs/EmployeePermissions.csv");
+    PermissionTbl.getInstance().loadBackup("BackupsCSVs/PermissionsForTesting.csv");
     CredentialsTbl.getInstance(); // .loadBackup("BackupsCSVs/CredentialsTbl.csv");
     AudioVisualTbl.getInstance();
     ComputerTbl.getInstance();
@@ -59,7 +60,7 @@ public class Main {
 
     PathTbl.getInstance(); // .loadBackup("backups/AllLocationEdges.csv");
 
-    EmployeePermissionTbl.getInstance(); // .loadBackup("BackupsCSVs/employeePermissionsTbl.csv");
+    EmployeePermissionTbl.getInstance().loadBackup("BackupsCSVs/employeePermissionsTbl.csv");
     //    CredentialsTbl.getInstance().addEntry(new Credential(456, "p"));
     //    CredentialsTbl.getInstance().addEntry(new Credential(666, "p"));
     //    CredentialsTbl.getInstance().addEntry(new Credential(123, "p"));
@@ -77,6 +78,9 @@ public class Main {
     //    EmployeePermission staffPerm = new EmployeePermission(456, 222);
     //    EmployeePermissionTbl.getInstance().addEntry(adminPerm);
     //    EmployeePermissionTbl.getInstance().addEntry(staffPerm);
+
+    CredentialsTbl.getInstance().addEntry(new Credential(0, "admin"));
+    CredentialsTbl.getInstance().addEntry(new Credential(123, "password"));
     floorMaps.load();
     App.launch(App.class, args);
 
