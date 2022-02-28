82619a8c1d7f35a52e92a25ae16cc0010f02469c
Michael O'Connor <michaeloconnor1717@gmail.com>
2022-02-27 22:56:14 -0500

    temp

--- src/main/java/edu/wpi/GoldenGandaberundas/Main.java
+++ src/main/java/edu/wpi/GoldenGandaberundas/Main.java
@@ -9,8 +9,10 @@ import edu.wpi.GoldenGandaberundas.tableControllers.EmployeeObjects.CredentialsT
 import edu.wpi.GoldenGandaberundas.tableControllers.EmployeeObjects.EmployeeTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.FoodService.FoodTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.GiftDeliveryService.GiftTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.LaundryService.LaundryRequestTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.LaundryService.LaundryTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.Locations.LocationTbl;
+import edu.wpi.GoldenGandaberundas.tableControllers.MedEquipmentDelivery.MedEquipRequestTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.MedEquipmentDelivery.MedEquipmentTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.MedicineDeliveryService.MedicineTbl;
 import edu.wpi.GoldenGandaberundas.tableControllers.Patients.PatientTbl;
@@ -54,73 +56,17 @@ public class Main {
     FoodTbl.getInstance().loadBackup("BackupsCSVs/foodTbl.csv");
     PathTbl.getInstance().loadBackup("BackupsCSVs/pathTbl.csv");
 
-    //    EmployeeTbl.getInstance().loadBackup("BackupsCSVs/employeeTbl.csv");
-    //    LocationTbl.getInstance().loadBackup("BackupsCSVs/locationTbl.csv");
-    //    PatientTbl.getInstance().loadBackup("BackupsCSVs/patientTbl.csv");
-    //    LaundryTbl.getInstance().loadBackup("BackupsCSVs/laundryTbl.csv");
-    //    MedEquipmentTbl.getInstance().loadBackup("BackupsCSVs/medEquipmentTbl.csv");
-    //    MedicineTbl.getInstance().loadBackup("BackupsCSVs/medicineTbl.csv");
-    //    GiftTbl.getInstance().loadBackup("BackupsCSVs/giftTbl.csv");
-    //    PermissionTbl.getInstance().loadBackup("BackupsCSVs/PermissionsForTesting.csv");
-    //    CredentialsTbl.getInstance().loadBackup("BackupsCSVs/CredentialsTbl.csv");
-    //    AudioVisualTbl.getInstance().loadBackup("BackupsCSVs/audioVisualTbl.csv");
-    //    ComputerTbl.getInstance().loadBackup("BackupsCSVs/computerTbl.csv");
-    //    PathTbl.getInstance().loadBackup("BackupsCSVs/pathTbl.csv");
-    //    FoodTbl.getInstance().loadBackup("BackupsCSVs/foodTbl.csv");
-    //    //    LocationTbl.getInstance().loadBackup("BackupsCSVs/locationTbl.csv");
-    //    //    PathTbl.getInstance().loadBackup("BackupsCSVs/")
-
     GiftRequestTbl.getInstance();
     MedicineRequestTbl.getInstance();
-    MedEquipRequestTbl.getInstance();
-    LaundryRequestTbl.getInstance();
     FoodRequestTbl.getInstance();
     ComputerRequestTbl.getInstance();
     AudioVisualRequestTbl.getInstance();
 
-    //    Food yummyFood = new Food(1, "Chicken", "Chicken and Rice", 300, "Meat", 10.99, true,
-    // "Dinner");
-    //    FoodTbl.getInstance().addEntry(yummyFood);
 
-    PathTbl.getInstance(); // .loadBackup("backups/AllLocationEdges.csv");
-    EmployeeTbl.getInstance(); // .loadBackup("BackupsCSVs/employeeTbl.csv");
-    EmployeePermissionTbl.getInstance(); // .loadBackup("BackupsCSVs/employeePermissionsTbl.csv");
+    MedEquipRequestTbl.getInstance().loadBackup("backups/medEquipReqTbl.csv");
+    LaundryRequestTbl.getInstance().loadBackup("backups/LaundryRequest.csv");
 
-    //        CredentialsTbl.getadminInstance().addEntry(new Credential(456, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(666, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(123, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(777, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(888, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(999, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(70, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(420, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(69, "p"));
-    //        CredentialsTbl.getInstance().addEntry(new Credential(96, "p"));
-
-    AudioVisualTbl.getInstance(); // .loadBackup("BackupsCSVs/BackupAudioVisualTbl.csv");
-    PathTbl.getInstance(); // .loadBackup("BackupsCSVs/pathTbl.csv");
-
-    MedEquipmentTbl.getInstance(); // .loadBackup("TestCSVs/medEquipSimulation.csv");
-    ArrayList<Point> points = LocationTbl.getInstance().getNodes();
-    points = PathTbl.getInstance().createBranchedLocations(points);
-
-    EmployeeTbl.getInstance()
-        .addEntry(
-            new Employee(0, "admin", "admin", "admin", "admin@bhm.org", "1111111111", "I live"));
-    EmployeeTbl.getInstance()
-        .addEntry(
-            new Employee(1, "staff", "staff", "staff", "staff@bhm.org", "2222222222", "I die"));
-    CredentialsTbl.getInstance().addEntry(new Credential(0, "admin"));
-    CredentialsTbl.getInstance().addEntry(new Credential(123, "password"));
-    //    System.out.println(CredentialsTbl.getInstance().getEntry(0).checkPassword("admin"));
-    CredentialsTbl.getInstance().addEntry(new Credential(1, "staff"));
-    CredentialsTbl.getInstance().addEntry(new Credential(999, "999"));
-    CredentialsTbl.getInstance().addEntry(new Credential(666, "666"));
-    EmployeePermissionTbl.getInstance().addEntry(new EmployeePermission(0, 111));
-
-    //    Simulation.update();
-    PathTbl.createStatsMap();
-
+    ConnectionHandler.getInstance().setConnection(ConnectionType.clientServer);
 
     //    floorMaps.load();
     //    App.launch(App.class, args);
